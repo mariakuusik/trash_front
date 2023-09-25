93f3e66224e344d5b212ced6372d0c9a0438e323
mariakuusik <maria.kuusik@gmail.com>
2023-09-25 20:34:23 +0300

    Intitial commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from "vue-axios";
+import axios from "axios";
 
 createApp(App).use(router).mount('#app')
+
+app.use(router)
+app.use(VueAxios, axios)
+app.mount('#app')
